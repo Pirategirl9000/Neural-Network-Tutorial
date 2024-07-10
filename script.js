ae7306d8eec403cc9dbb7a844ac26a7b9975724c
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2024-07-10 02:04:54 +0000

    draw_neural_network() done

--- script.js
+++ script.js
@@ -2,9 +2,34 @@ function draw_neural_network() {
     canvas = document.getElementById("nn");
     ctx = canvas.getContext('2d');
 
+    //Input
     ctx.beginPath();
     ctx.arc(75, 75, 40, 0, 2*Math.PI);
-    ctx.lineTo(700, 75)
+    ctx.lineTo(200, 75);
+    ctx.moveTo(240, 75);
     ctx.stroke();
     ctx.closePath();
+
+    //Hidden Layer 1
+    ctx.beginPath();
+    ctx.arc(240, 75, 40, 0, 2*Math.PI);
+    ctx.lineTo(365, 75);
+    ctx.moveTo(405, 75);
+    ctx.stroke();
+    ctx.closePath();
+
+    //Hidden Layer 2
+    ctx.beginPath();
+    ctx.arc(405, 75, 40, 0, 2*Math.PI);
+    ctx.lineTo(530, 75);
+    ctx.moveTo(570, 75)
+    ctx.stroke();
+    ctx.closePath();
+
+    //Output Layer
+    ctx.beginPath();
+    ctx.arc(570, 75, 40, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.closePath();
+
 }
