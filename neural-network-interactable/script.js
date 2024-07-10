85f73c6638e90752ef2b79c1f6d1ea5e741ac3bf
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2024-07-10 20:47:41 +0000

    Started adding subpage for interactable nn

---
+++ neural-network-interactable/script.js
@@ -0,0 +1,15 @@
+function run_test_nn() {
+    let input = document.getElementById("feature").innerHTML;
+    let output = document.getElementById("label").innerHTML;
+    let w1 = document.getElementById("weight_1").innerHTML;
+    let b1 = document.getElementById("bias_1").innerHTML;
+    let a1 = document.getElementById("act_1").innerHTML;
+    let w2 = document.getElementById("weight_2").innerHTML;
+    let b2 = document.getElementById("bias_2").innerHTML;
+    let a2 = document.getElementById("act_2").innerHTML;
+    let w3 = document.getElementById("weight_3").innerHTML;
+    let b3 = document.getElementById("bias_3").innerHTML;
+    let a3 = document.getElementById("act_3").innerHTML;
+    let epochs = document.getElementById("epochs").innerHTML;
+    let learning_rate = document.getElementById("learning_rate").innerHTML;
+}
