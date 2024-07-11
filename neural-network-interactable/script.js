b26813906b855ba4ef730f006e70348f7887057f
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2024-07-11 02:49:42 +0000

    parsed inputs

--- neural-network-interactable/script.js
+++ neural-network-interactable/script.js
@@ -84,6 +84,35 @@ function run_nn() {
     let learning_rate = parseFloat(document.getElementById("learning_rate").value);
     let display = document.getElementById("display");
 
-    //Swap activation function from string to function reference
+    //Swap activation function from string to function reference and get it's derivative reference
+    let references_1 = get_activation_reference(a1);
+    let references_2 = get_activation_reference(a2);
+    let references_3 = get_activation_reference(a3);
 
+    //JS can't do multiple assignment like Python so we gotta do this garbage. From 3 lines to 9, Python > JS
+    a1 = references_1[0];
+    d1 = references_1[1];
+    a2 = references_2[0];
+    d2 = references_2[1];
+    a3 = references_3[0];
+    d3 = references_3[1];
+
+}
+
+function get_activation_reference(input) {
+    let activation;
+    let derivative;
+
+    if (input == "Sigmoid") {
+        activation = sigmoid;
+        derivative = sigmoid_der;
+    } else if (input == "ReLU") {
+        activation = relu;
+        derivative = relu_der;
+    } else {
+        activation = tanh;
+        derivative = tanh_der;
+    }
+
+    return [activation, derivative];
 }
